21575925b39719d0cf056b96369434a16cf8c01c
Philipp Rouast <philipp@rouast.com>
2026-02-26 10:27:09 +1100

    Create Swift package

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "VitalLensCore",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "VitalLensCore",
+            targets: ["VitalLensCore"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "VitalLensCore",
+            dependencies: ["VitalLensCoreFFI"],
+            path: "bindings/swift" 
+        ),
+        .binaryTarget(
+            name: "VitalLensCoreFFI",
+            path: "target/VitalLensCoreFFI.xcframework"
+        )
+    ]
+)
