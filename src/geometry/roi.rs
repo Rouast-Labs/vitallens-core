d791a47f867497aac8387cb306dccbd055ded990
Philipp Rouast <philipp@rouast.com>
2026-02-26 10:24:53 +1100

    Update Apple Vision defaults

--- src/geometry/roi.rs
+++ src/geometry/roi.rs
@@ -13,7 +13,7 @@ const UPPER_BODY_OFFSETS: [f32; 4] = [0.25, 0.20, 0.25, 0.40];
 const UPPER_BODY_CROPPED_OFFSETS: [f32; 4] = [0.19, 0.1455, 0.19, 0.2769];
 
 /// Offsets to convert other Face Detectors to default
-const VISION_TO_DEFAULT_OFFSETS: [f32; 4] = [-0.05, 0.20, -0.05, 0.25];
+const VISION_TO_DEFAULT_OFFSETS: [f32; 4] = [-0.05, 0.1, -0.05, 0.25];
 
 #[cfg_attr(not(target_arch = "wasm32"), uniffi::export)]
 pub fn calculate_roi(
